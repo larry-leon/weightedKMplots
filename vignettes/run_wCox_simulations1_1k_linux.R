9582f4abedf112540448204e44be89c8f6535e37
Larry Leon <leonhsph@gmail.com>
2025-09-20 02:07:02 -0700

    linux sims

--- vignettes/run_wCox_simulations1_1k_linux.R
+++ vignettes/run_wCox_simulations1_1k_linux.R
@@ -107,7 +107,7 @@ ans$dropout_rate <- dropout_rate
 return(ans)
 }
 
-n_now <- 5000
+n_now <- 1000
 
 these_sims <- get_sims(n_sim = n_now, seedstart = 8316951, num_workers = 100, mart_draws = 500, save_results = TRUE,
                        file_togo = "vignettes/results/wCoxsims1_linux_1k.RData")
